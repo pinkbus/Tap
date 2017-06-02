f7b8ac3dbdd51c1ddbc9bda928dbfd7ade913376
Le Hoang Vinh <victorhwang95@gmail.com>
2017-06-01 21:57:05 -0700

    Tapme commit

--- Tap/ViewController.h
+++ Tap/ViewController.h
@@ -5,10 +5,23 @@
 //  Created by Le Hoang Vinh on 5/11/17.
 //  Copyright © 2017 Le Hoang Vinh. All rights reserved.
 //
-
+#import <AVFoundation/AVFoundation.h>
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIAlertViewDelegate>{
+IBOutlet UILabel *timerLabel;
+IBOutlet UILabel *scoreLabel;
+    
+    NSInteger count;
+    NSInteger second;
+    NSTimer *timer;
+    
+    //Add these AVAudioPlayer object!!
+    AVAudioPlayer *buttonBeep;
+    AVAudioPlayer *secondBeep;
+    AVAudioPlayer *backgroundMusic;
+}
+-(IBAction) buttonPressed;
 
 
 @end
--- Tap/ViewController.m
+++ Tap/ViewController.m
@@ -17,13 +17,82 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"bg_title.png"]];
+    scoreLabel.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"field_score.png"]];
+    timerLabel.backgroundColor=[UIColor colorWithPatternImage:[UIImage imageNamed:@"field_time.png"]];
+    
+    buttonBeep=[self setupAudioPlayerWithFile: @"ButtonTap" type:@"wav"];
+    secondBeep=[self setupAudioPlayerWithFile: @"SecondBeep" type:@"wav"];
+    backgroundMusic=[self setupAudioPlayerWithFile:@"HallOfTheMountainKing" type:@"mp3"];
+    [self setup];
 }
 
-
+-(AVAudioPlayer *)setupAudioPlayerWithFile: (NSString*) file type:(NSString *)type
+{
+    NSString *path=[[NSBundle mainBundle] pathForResource:file ofType: type];
+    NSURL *url=[NSURL fileURLWithPath:path];
+    //
+    NSError *error;
+    //
+    AVAudioPlayer *audioPlayer=[[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    //
+    if(!audioPlayer)
+    {
+        NSLog(@"%@ ",[error description]);
+    }
+    
+    return audioPlayer;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+-(IBAction)buttonPressed
+{
+    count++;
+    scoreLabel.text=[NSString stringWithFormat:@"Socre\n%li",count];
+    [buttonBeep play];
+}
+-(void) setup{
+    count=0;
+    second=30;
+    
+    timerLabel.text=[NSString stringWithFormat: @"Timer: %li",second];
+    scoreLabel.text=[NSString stringWithFormat: @"Score \n%li",count];
+    
 
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0f
+                                             target:self
+                                           selector:@selector(subtractTime)
+                                           userInfo:nil
+                                            repeats:YES];
+    [backgroundMusic play];
+    [backgroundMusic setVolume:0.3];
+    
+}
+-(void)subtractTime
+{
+    //1
+    second--;
+    [secondBeep play];
+    timerLabel.text=[NSString stringWithFormat: @"Timer: %li",second];
+    
+    if(second==0)
+    {
+        [timer invalidate];
+        UIAlertController *alert=[UIAlertController alertControllerWithTitle: @"Time's Up"
+                                                                     message:[NSString stringWithFormat:@"Your Score is %li",count]
+                                                              preferredStyle: UIAlertControllerStyleAlert];
+        UIAlertAction *ok=[UIAlertAction actionWithTitle:@"Play Again!!"
+                                                   style: UIAlertActionStyleDefault
+                                                 handler:^(UIAlertAction *action)
+                           {
+                               [self setup];
+                               [alert dismissViewControllerAnimated:YES completion:nil];
+                           }];
+        [alert addAction: ok];
+        [self presentViewController: alert animated:YES completion: nil];
+    }
+}
 @end
